4fa67c2efb614a6a728a780cb11299434c3782b2
jbeltrami <joao_beltrami@hotmail.com>
2017-06-13 11:08:04 -0400

    Add event handler to index.js
    Add functionality to test authentication related activities.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,11 +2,16 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const authEvents = require('./auth/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
+  authEvents.addHandlers()
 })
 
+// $(() => {
+//   authEvents.addHandlers()
+// })
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
