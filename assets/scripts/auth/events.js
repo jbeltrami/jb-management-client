2003a79517a496ddca49befc75a7ace8e2a803d4
jbeltrami <joao_beltrami@hotmail.com>
2017-06-13 12:09:53 -0400

    Create CRUD actions for Clients
    api.js - Create AJAX requests for all CRUD actions related to clients.
    events.js - Create JQuery click events to trigger API calls.
    ui.js - Create functions to handle API response (yet to add code).
    index.html - Write throwaway code to test API calls.
    
    next step: create CRUD actions for Services

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -37,11 +37,56 @@ const onSignOut = function (event) {
     .catch(ui.signOutFailure)
 }
 
+const onAddClient = function (event) {
+  const data = getFormFields(event.target)
+  event.preventDefault()
+  api.addClient(data)
+    .then(ui.addClientSuccess)
+    .catch(ui.addClientFailure)
+}
+
+const onGetClient = function (event) {
+  const data = getFormFields(event.target)
+  event.preventDefault()
+  api.getClient(data)
+    .then(ui.getClientSuccess)
+    .catch(ui.getClientFailure)
+}
+
+const onGetClientIndex = function (event) {
+  const data = getFormFields(event.target)
+  event.preventDefault()
+  api.getClientIndex(data)
+    .then(ui.getClientIndexSuccess)
+    .catch(ui.getClientIndexFailure)
+}
+
+const onUpdateClient = function (event) {
+  const data = getFormFields(event.target)
+  event.preventDefault()
+  api.updateClient(data)
+    .then(ui.updateClientSuccess)
+    .catch(ui.updateClientFailure)
+}
+
+const onDestroyClient = function (event) {
+  const data = getFormFields(event.target)
+  event.preventDefault()
+  api.destroyClient(data)
+    .then(ui.destroyClientSuccess)
+    .catch(ui.destroyClientFailure)
+}
+
 const addHandlers = () => {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#changepw').on('submit', onChangePW)
   $('#sign-out').on('submit', onSignOut)
+  $('#add-client').on('submit', onAddClient)
+  $('#get-client').on('submit', onGetClient)
+  $('#get-client-index').on('submit', onGetClientIndex)
+  $('#update-client').on('submit', onUpdateClient)
+  $('#destroy-client').on('submit', onDestroyClient)
 }
 
 module.exports = {
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,37 +2,90 @@
 
 const store = require('../store.js')
 
-const signUpSuccess = (data) => {
+const resetFields = function () {
+  document.getElementById('sign-up').reset()
+  document.getElementById('sign-in').reset()
+  document.getElementById('changepw').reset()
+  document.getElementById('sign-out').reset()
+  document.getElementById('add-client').reset()
+  document.getElementById('get-client').reset()
+  document.getElementById('get-client-index').reset()
+  document.getElementById('update-client').reset()
+  document.getElementById('destroy-client').reset()
+}
 
+const signUpSuccess = (data) => {
+  resetFields()
 }
 
 const signUpFailure = (data) => {
-
+  resetFields()
 }
 
 // makes all the buttons appear.
 const signInSuccess = (data) => {
+  resetFields()
   store.user = data.user
 }
 
 const signInFailure = (data) => {
-
+  resetFields()
 }
 
 const changePWSuccess = (data) => {
-
+  resetFields()
 }
 
 const changePWFailure = (data) => {
-
+  resetFields()
 }
 
 const signOutSuccess = (data) => {
-
+  resetFields()
 }
 
 const signOutFailure = (data) => {
+  resetFields()
+}
 
+const addClientSuccess = (data) => {
+  resetFields()
+}
+
+const addClientFailure = (data) => {
+  resetFields()
+}
+
+const getClientSuccess = (data) => {
+  resetFields()
+}
+
+const getClientFailure = (data) => {
+  resetFields()
+}
+
+const getClientIndexSuccess = (data) => {
+  resetFields()
+}
+
+const getClientIndexFailure = (data) => {
+  resetFields()
+}
+
+const updateClientSuccess = (data) => {
+  resetFields()
+}
+
+const updateClientFailure = (data) => {
+  resetFields()
+}
+
+const destroyClientSuccess = (data) => {
+  resetFields()
+}
+
+const destroyClientFailure = (data) => {
+  resetFields()
 }
 
 module.exports = {
@@ -43,5 +96,15 @@ module.exports = {
   changePWSuccess,
   changePWFailure,
   signOutSuccess,
-  signOutFailure
+  signOutFailure,
+  addClientSuccess,
+  addClientFailure,
+  getClientSuccess,
+  getClientFailure,
+  getClientIndexSuccess,
+  getClientIndexFailure,
+  updateClientSuccess,
+  updateClientFailure,
+  destroyClientSuccess,
+  destroyClientFailure
 }
