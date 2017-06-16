0bcf5f69dac8a4ba9c65b5ff1e8a414b100e4a10
jbeltrami <joao_beltrami@hotmail.com>
2017-06-15 23:20:41 -0400

    Add production URL to api config file
    Api calls now point to the deployed version of the server

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    // production: 'https://ga-wdi-boston.herokuapp.com',
+    production: 'https://jb-management.herokuapp.com/',
     development: 'http://localhost:4741'
   }
 }
